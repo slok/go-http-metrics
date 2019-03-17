4fb522ec10485fe12e42e70304b2c8466dc85468
Xabier Larrakoetxea <slok69@gmail.com>
2019-03-17 17:44:19 +0100

    Add example for default case
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ examples/default/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
+	"github.com/slok/go-http-metrics/middleware"
+)
+
+const (
+	srvAddr     = ":8080"
+	metricsAddr = ":8081"
+)
+
+// this example will show the simplest way of enabling the middleware
+// using go std handlers without frameworks with the Prometheues recorder.
+// it uses the prometheus default registry, the middleware default options
+// and doesn't set the handler ID, so the middleware will set the handler id
+// (handler label) to the request URL, WARNING: this creates high cardinality because
+// `/profile/123` and `/profile/567` are different handlers. If you want to be safer
+// you will need to tell the middleware what is the handler id.
+func main() {
+	// Create our middleware.
+	mdlw := middleware.New(middleware.Config{
+		Recorder: metrics.New(metrics.Config{}),
+	})
+
+	// Create our server.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
+	mux.HandleFunc("/test1", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusCreated) })
+	mux.HandleFunc("/test1/test2", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusAccepted) })
+	mux.HandleFunc("/test1/test4", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusNonAuthoritativeInfo) })
+	mux.HandleFunc("/test2", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusNoContent) })
+	mux.HandleFunc("/test3", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusResetContent) })
+
+	// Wrap our main handler, we pass empty handler ID so the middleware inferes
+	// the handler label from the URL.
+	h := mdlw.Handler("", mux)
+
+	// Serve our handler.
+	go func() {
+		log.Printf("server listening at %s", srvAddr)
+		if err := http.ListenAndServe(srvAddr, h); err != nil {
+			log.Panicf("error while serving: %s", err)
+		}
+	}()
+
+	// Serve our metrics.
+	go func() {
+		log.Printf("metrics listening at %s", metricsAddr)
+		if err := http.ListenAndServe(metricsAddr, promhttp.Handler()); err != nil {
+			log.Panicf("error while serving metrics: %s", err)
+		}
+	}()
+
+	// Wait until some signal is captured.
+	sigC := make(chan os.Signal, 1)
+	signal.Notify(sigC, syscall.SIGTERM, syscall.SIGINT)
+	<-sigC
+}
---
+++ middleware/example_test.go
@@ -0,0 +1,40 @@
+package middleware_test
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
+	"github.com/slok/go-http-metrics/middleware"
+)
+
+// PrometheusBackendMiddleware shows how you would create a middleware factory for standard
+// go library `http.Handler` and wrap a handler to measure with the default settings using
+// Prometheus as the metrics recorder backend, the Prometheus will use the default settings
+// so it will measure using the default Prometheus registry.
+func ExampleMiddleware_prometheusBackendMiddleware() {
+	// Create our middleware factory with the default settings.
+	mdlw := middleware.New(middleware.Config{
+		Recorder: metrics.New(metrics.Config{}),
+	})
+
+	// Create our handler.
+	myHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello world!"))
+	})
+
+	// Wrap our handler with the middleware.
+	h := mdlw.Handler("", myHandler)
+
+	// Serve metrics from the default prometheus registry.
+	log.Printf("serving metrics at: %s", ":8081")
+	go http.ListenAndServe(":8081", promhttp.Handler())
+
+	// Serve our handler.
+	log.Printf("listening at: %s", ":8080")
+	if err := http.ListenAndServe(":8080", h); err != nil {
+		log.Panicf("error while serving: %s", err)
+	}
+}
