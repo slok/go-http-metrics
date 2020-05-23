7978a37f4771c7739c7b05741a78d4b20921a1ca
Trevor Foster <trevor.foster@hotmail.ca>
2020-05-23 01:16:09 -0400

    add support for echo

---
+++ middleware/echo/echo.go
@@ -0,0 +1,20 @@
+// Package echo is a helper package to get an echo compatible
+// handler/middleware from the standard net/http Middleware factory.
+package echo
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/slok/go-http-metrics/middleware"
+)
+
+// Handler returns a Echo compatible middleware from a Middleware factory instance.
+// The first handlerID argument is the same argument passed on Middleware.Handler method.
+func Handler(handlerID string, m middleware.Middleware) echo.MiddlewareFunc {
+	// Create a dummy handler to wrap the middleware chain of Gin, this way Middleware
+	// interface can wrap the Gin chain.
+	return echo.WrapMiddleware(func(next http.Handler) http.Handler {
+		return m.Handler(handlerID, next)
+	})
+}
---
+++ middleware/echo/echo_test.go
@@ -0,0 +1,81 @@
+package echo_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	mmetrics "github.com/slok/go-http-metrics/internal/mocks/metrics"
+	"github.com/slok/go-http-metrics/metrics"
+	"github.com/slok/go-http-metrics/middleware"
+	echoMiddleware "github.com/slok/go-http-metrics/middleware/echo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func getTestHandler(statusCode int) echo.HandlerFunc {
+	return func(context echo.Context) error {
+		context.Response().WriteHeader(statusCode)
+		return nil
+	}
+}
+
+func TestMiddlewareIntegration(t *testing.T) {
+	tests := []struct {
+		name          string
+		handlerID     string
+		statusCode    int
+		req           *http.Request
+		config        middleware.Config
+		expHandlerID  string
+		expService    string
+		expMethod     string
+		expStatusCode string
+	}{
+		{
+			name:          "A default HTTP middleware should call the recorder to measure.",
+			statusCode:    http.StatusAccepted,
+			req:           httptest.NewRequest(http.MethodPost, "/test", nil),
+			expHandlerID:  "/test",
+			expMethod:     http.MethodPost,
+			expStatusCode: "202",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mocks.
+			mr := &mmetrics.Recorder{}
+			expHTTPReqProps := metrics.HTTPReqProperties{
+				ID:      test.expHandlerID,
+				Service: test.expService,
+				Method:  test.expMethod,
+				Code:    test.expStatusCode,
+			}
+			expHTTPProps := metrics.HTTPProperties{
+				ID:      test.expHandlerID,
+				Service: test.expService,
+			}
+			mr.On("ObserveHTTPRequestDuration", mock.Anything, expHTTPReqProps, mock.Anything).Once()
+			mr.On("ObserveHTTPResponseSize", mock.Anything, expHTTPReqProps, mock.Anything).Once()
+			mr.On("AddInflightRequests", mock.Anything, expHTTPProps, 1).Once()
+			mr.On("AddInflightRequests", mock.Anything, expHTTPProps, -1).Once()
+
+			// Create our negroni instance with the middleware.
+			mdlw := middleware.New(middleware.Config{Recorder: mr})
+			e := echo.New()
+			e.POST("/test", getTestHandler(test.statusCode), echoMiddleware.Handler("", mdlw))
+
+			// Make the request.
+			resp := httptest.NewRecorder()
+			e.ServeHTTP(resp, test.req)
+
+			// Check.
+			mr.AssertExpectations(t)
+			assert.Equal(test.statusCode, resp.Result().StatusCode)
+		})
+	}
+}
