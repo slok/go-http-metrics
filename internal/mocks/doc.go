48f7cbaddfc87d31b4a569405b9ec6ecbe79beed
sashayakovtseva <sashayakovtseva@gmail.com>
2021-06-17 18:18:41 +0300

    Support custom labels

--- internal/mocks/doc.go
+++ internal/mocks/doc.go
@@ -3,5 +3,6 @@ Package mocks will have all the mocks of the library.
 */
 package mocks // import "github.com/slok/go-http-metrics/internal/mocks"
 
-//go:generate mockery -output ./metrics -outpkg metrics -dir ../../metrics -name Recorder
-//go:generate mockery -output ./middleware -outpkg middleware -dir ../../middleware -name Reporter
+//go:generate mockery --output ./metrics --outpkg metrics --dir ../../metrics --name Recorder
+//go:generate mockery --output ./middleware --outpkg middleware --dir ../../middleware --name Reporter
+//go:generate mockery --output ./middleware --outpkg middleware --dir ../../middleware --name CustomLabelReporter
